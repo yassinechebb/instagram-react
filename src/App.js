affa3fcecc48e205cfac0c29764f54ae9191addd
yassinechebb <chebbiyassine05@gmail.com>
2024-10-01 13:49:41 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Navbarr/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MainVideo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MainVideo() {
+  return (
+    <div>MainVideo</div>
+  )
+}
+
+export default MainVideo
